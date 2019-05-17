75b0093a691345b68c39ed97806420ba7aca0f0d
Murtuza <murtuza.labs@gmail.com>
2019-05-17 12:18:46 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,41 @@
+// Name of the input file should be "uploadVideo" in the HTML form
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+// set storage
+const storage = multer.diskStorage({
+    destination:'./uploads',
+    filename: function(req,file,callback){
+        // uploadView-timestamp.ext
+        callback(null,file.fieldname + "-" + Date.now() + path.extname(file.originalname))
+    }
+});
+
+// init upload
+const upload = multer({
+    storage: storage
+}).single('uploadVideo');
+
+app.post('/api/upload', (req,res)=>{
+    upload(req,res,(err)=>{
+        let status = {
+            success:false,
+            filename:"",
+            message:""
+        };
+        if(err){
+            status.message=err.code;
+        }else{
+            status.filename=req.file.filename;
+            status.success=true;
+            status.message="successfully uploaded";
+        }
+        res.send(JSON.stringify(status))
+    });
+});
+
+app.listen(port, ()=> console.log('Server started'));
